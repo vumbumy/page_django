3d2399e2e0e901ac57e56f96e82bd39d38533e14
Hanbeom Seo <vumbumy@outlook.kr>
2020-08-27 22:17:05 +0900

    Add Booking API for studying
     Date:      Thu Aug 27 22:17:05 2020 +0900

---
+++ booking/views.py
@@ -0,0 +1,15 @@
+from rest_framework import generics
+
+# Create your views here.
+from booking.models import Booking
+from booking.serializers import BookingSerializer
+
+
+class BookingList(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
